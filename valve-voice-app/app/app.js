153d61fee5d839feb02632da83e99aafc3da3d28
Sweetmantech <sweetmantech@gmail.com>
2018-08-24 21:56:40 -0400

    added Morpheus Quotes

--- valve-voice-app/app/app.js
+++ valve-voice-app/app/app.js
@@ -12,48 +12,57 @@ const config = {
 
 const app = new App(config);
 
+const AGENT_SMITH_URL = [
+  "https://s3.amazonaws.com/matrix-sounds/AgentSmith/A+man+who+calls+himself+Morpheus.mp3",
+  "https://s3.amazonaws.com/matrix-sounds/AgentSmith/Billions+of+people+living+up+their+lives.mp3",
+  "https://s3.amazonaws.com/matrix-sounds/AgentSmith/Goodbye.mp3",
+  "https://s3.amazonaws.com/matrix-sounds/AgentSmith/Have+you+ever+stood+and+stared.mp3",
+  "https://s3.amazonaws.com/matrix-sounds/AgentSmith/Human+beings+are+a+disease.mp3",
+  "https://s3.amazonaws.com/matrix-sounds/AgentSmith/Human+defines+their+reality+through+misery.mp3",
+  "https://s3.amazonaws.com/matrix-sounds/AgentSmith/I+hate+this+place.mp3",
+  "https://s3.amazonaws.com/matrix-sounds/AgentSmith/I+must+get+free.mp3",
+  "https://s3.amazonaws.com/matrix-sounds/AgentSmith/I+must+get+out+of+here.mp3",
+  "https://s3.amazonaws.com/matrix-sounds/AgentSmith/I+think+we+can+handle+one+little+girl.mp3",
+  "https://s3.amazonaws.com/matrix-sounds/AgentSmith/Look+out+that+window.mp3",
+  "https://s3.amazonaws.com/matrix-sounds/AgentSmith/Marvel+and+its+beauty+its+genius.mp3",
+  "https://s3.amazonaws.com/matrix-sounds/AgentSmith/Matrix+designed+to+be+perfect.mp3",
+  "https://s3.amazonaws.com/matrix-sounds/AgentSmith/Mr.+Anderson.mp3",
+  "https://s3.amazonaws.com/matrix-sounds/AgentSmith/Never+send+a+human+to+do+a+machines+job.mp3",
+  "https://s3.amazonaws.com/matrix-sounds/AgentSmith/No+one+would+accept+the+program.mp3",
+  "https://s3.amazonaws.com/matrix-sounds/AgentSmith/Smith++Agent+Smith.mp3",
+  "https://s3.amazonaws.com/matrix-sounds/AgentSmith/Some+believed+we+lacked+the+language.mp3",
+  "https://s3.amazonaws.com/matrix-sounds/AgentSmith/Sound+of+inevitability.mp3",
+  "https://s3.amazonaws.com/matrix-sounds/AgentSmith/The+future+is+our+world.mp3",
+  "https://s3.amazonaws.com/matrix-sounds/AgentSmith/There+was+a+disaster.mp3",
+  "https://s3.amazonaws.com/matrix-sounds/AgentSmith/This+zoo+this+prison+this+reality+whatever.mp3",
+  "https://s3.amazonaws.com/matrix-sounds/AgentSmith/What+good+is+a+phone+call.mp3",
+  "https://s3.amazonaws.com/matrix-sounds/AgentSmith/You+had+your+time.mp3",
+  "https://s3.amazonaws.com/matrix-sounds/AgentSmith/Youre+going+to+help+us.mp3",
+  "https://s3.amazonaws.com/matrix-sounds/AgentSmith/Youve+been+contacted+by+a+certain+individual.mp3"
+]
+
 
 // =================================================================================
 // App Logic
 // =================================================================================
 
-const intentNames = {
-    WHICH_PILL: 'WhichPill',
-    WHICH_PILL_RESPONSE: "WhichPillResponse",
-}
-const PILLS = {
-    BLUE: 'blue',
-    RED: 'red',
-}
 app.setHandler({
     'LAUNCH': function() {
-        this.toIntent(intentNames.WHICH_PILL);
+        this.toIntent('HelloWorldIntent');
     },
 
     'HelloWorldIntent': function() {
         this.ask('Hello World! What\'s your name?', 'Please tell me your name.');
     },
-    [intentNames.WHICH_PILL]() {
-        this.ask('Which pill will you take? The blue pill or the red pill?', 'Tell me if you are going to take the red pill or the blue pill')
-    },
-    [intentNames.WHICH_PILL_RESPONSE](color) {
-        switch (color.value) {
-            case PILLS.RED: {
-                this.tell("you stay in Wonderland and I show you how deep the rabbit-hole goes")
-                break
-            }
-            case PILLS.BLUE: {
-                this.tell("The story has ended. You will wake up in your bed and believe whatever you want to believe")
-                break
-            }
-            default:
-            {
 
-                this.tell(`I\m not really sure what pill you took. You chose ${color} `)
-                break
-            }
-        }
-    }
+    'HelpIntent': function() {
+        this.ask('Hello World! What\'s your name?', 'Please tell me your name.');
+    },
+
+
+    'MyNameIsIntent': function( name ) {
+        this.tell('Hey ' + name.value + ', nice to meet you!');
+    },
 });
 
 module.exports.app = app;
