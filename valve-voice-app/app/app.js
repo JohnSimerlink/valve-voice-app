567ac447c51e8c7c19aeb9f327fd0c582d0c4d62
JohnSimerlink <johnsimerlink@gmail.com>
2018-08-24 20:37:18 -0400

    Add WhichPill and WhichPillResponse intents

--- valve-voice-app/app/app.js
+++ valve-voice-app/app/app.js
@@ -17,18 +17,43 @@ const app = new App(config);
 // App Logic
 // =================================================================================
 
+const intentNames = {
+    WHICH_PILL: 'WhichPill',
+    WHICH_PILL_RESPONSE: "WhichPillResponse",
+}
+const PILLS = {
+    BLUE: 'blue',
+    RED: 'red',
+}
 app.setHandler({
     'LAUNCH': function() {
-        this.toIntent('HelloWorldIntent');
+        this.toIntent(intentNames.WHICH_PILL);
     },
 
     'HelloWorldIntent': function() {
         this.ask('Hello World! What\'s your name?', 'Please tell me your name.');
     },
-
-    'MyNameIsIntent': function(name) {
-        this.tell('Hey ' + name.value + ', nice to meet you!');
+    [intentNames.WHICH_PILL]() {
+        this.ask('Which pill you take? The blue pill or the red pill?', 'Tell me if you are going to take the red pill or the blue pill')
     },
+    [intentNames.WHICH_PILL_RESPONSE](color) {
+        switch (color.value) {
+            case PILLS.RED: {
+                this.tell("you stay in Wonderland and I show you how deep the rabbit-hole goes")
+                break
+            }
+            case PILLS.BLUE: {
+                this.tell("The story has ended. You will wake up in your bed and believe whatever you want to believe")
+                break
+            }
+            default:
+            {
+                
+                this.tell(`I\m not really sure what pill you took. You chose ${color} `)
+                break
+            }
+        }
+    }
 });
 
 module.exports.app = app;
